b4cc2d4c18794e87fe1af227ed17da66492d8dfa
jggoncalez <joaogabriel.racer@gmail.com>
2025-10-28 16:54:48 -0300

    Add initial implementation of Flask app and Arduino sensor integration

---
+++ app.py
@@ -0,0 +1,42 @@
+import flask as fl
+import serial, time
+
+try:
+    arduino = serial.Serial('COM4',9600,timeout=1)
+    time.sleep(2)
+except serial.SerialException as e:  
+    print(f"Error: arduino not connected! {e}")
+    arduino = None
+
+app = fl.Flask(__name__)
+
+@app.route('/')
+def index():
+    return fl.render_template('index.html')
+
+
+@app.route('/get_data/')
+def getSensorData():
+    if not arduino:
+        return fl.jsonify({"erro": "Arduino não conectado"})
+
+    command = 'T'
+    arduino.write(command.encode())
+    if arduino.in_waiting > 0:
+        time.sleep(1)
+        line = arduino.readline().decode('utf-8', errors='replace').strip()
+        values = line.split(";")
+
+        if len(values) >= 3:
+            return fl.jsonify({
+                "temperature": values[0],
+                "humidity": values[1],
+                "lightIntensity": values[2],
+            })
+        else:
+            return fl.jsonify({"erro": f"No valid data detected: {line}"})
+    else:
+        return fl.jsonify({"error": "No Data Available"})
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader = False)
---
+++ arduino_code/sensor/sensor.ino
@@ -0,0 +1,38 @@
+#include "DHT.h"
+
+#define DHTPIN 3      
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
+#define LDRPIN A0 
+
+void setup() {
+    Serial.begin(9600);
+    dht.begin();
+    pinMode(LDRPIN, INPUT);
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        char command = Serial.read();
+        
+        switch (command) {  
+            case 'T':
+                float t = dht.readTemperature();
+                float h = dht.readHumidity();
+                int lightRaw = analogRead(LDRPIN);
+                int lightPercent = map(lightRaw, 0, 1023, 0, 100);
+                
+                // if (isnan(t) || isnan(h) || isnan(lightPercent)) {
+                //     Serial.println("ERROR");
+                // } else {
+                    Serial.print(t);
+                    Serial.print(";");
+                    Serial.print(h);
+                    Serial.print(";");
+                    Serial.println(lightPercent);
+                // }
+                break;
+        }
+    }
+}
